6e1772845bbed8a7efddf305c640f5694e5e6a38
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-02-26 11:57:46 -0500

    Updated to include NPDW records

--- epa/epa.R
+++ epa/epa.R
@@ -2,18 +2,18 @@
 # Packages ----------------------------------------------------------------
 
 {
-    library(rio)
-    library(janitor)
-    library(tidyverse)
-    library(here)
-    library(httr2)
-    library(rvest)
-    library(ComptoxR)
-    library(stringdist)
-    library(fuzzyjoin)
-    
-    setwd(here('epa'))
-    load('.Rdata')
+  library(rio)
+  library(janitor)
+  library(tidyverse)
+  library(here)
+  library(httr2)
+  library(rvest)
+  library(ComptoxR)
+  library(stringdist)
+  library(fuzzyjoin)
+  
+  setwd(here('epa'))
+  #load('.Rdata')
 }
 
 # functions ---------------------------------------------------------------
@@ -56,6 +56,36 @@ srs_details <- function(query){
 }
 
 
+# National Primary Drinking Water Regulations -----------------------------
+
+dw_headers <- read_html('https://www.epa.gov/ground-water-and-drinking-water/national-primary-drinking-water-regulations') %>%
+  html_elements(., 'h3') %>%
+  html_text()
+
+dw <- read_html('https://www.epa.gov/ground-water-and-drinking-water/national-primary-drinking-water-regulations') %>% 
+  html_elements(., 'table') %>% 
+  html_table() %>% 
+  set_names(., dw_headers) %>% 
+  map(., ~{
+    as_tibble(.x) %>%
+      select(1:3) %>% 
+      `colnames<-`(., c('analyte', 'mclg', 'mcl')) %>% 
+      mutate(across(everything(), as.character))
+  }, .progress = TRUE) %>% 
+  list_rbind(names_to = 'source') %>% 
+  pivot_longer(., cols = c(mclg, mcl), names_to = 'name', values_to = 'value') %>% 
+  mutate(
+    unit = 'mg/L',
+    analyte = str_remove_all(analyte, pattern = "\n.*")
+    # orig_value = value,
+    # value = as.numeric(value),
+    # value = case_when(
+    #   orig_value == 'zero' ~ 0,
+    #   .default = value)
+  )
+
+rm(dw_headers)
+
 # Aquatic -----------------------------------------------------------------
 
 
@@ -66,11 +96,8 @@ alc <- read_html('https://www.epa.gov/wqc/national-recommended-water-quality-cri
   `colnames<-`(., c('analyte', 'casrn', 'fw_ac', 'fw_chr', 'sw_ac', 'sw_chr', 'year', 'notes')) %>% 
   pivot_longer(., cols = fw_ac:sw_chr) %>% 
   mutate(
-    unit = 'ug/l',
-    cas_chk = webchem::as.cas(casrn)
+    unit = 'ug/l'
   )
-
-
 
 # Human -------------------------------------------------------------------
 
@@ -82,8 +109,7 @@ hhc <- read_html('https://www.epa.gov/wqc/national-recommended-water-quality-cri
   `colnames<-`(., c('analyte', 'casrn','hh_wo', 'hh_o', 'year', 'notes')) %>% 
   pivot_longer(., cols = c('hh_wo', 'hh_o')) %>% 
   mutate(
-    unit = 'ug/l',
-    cas_chk = webchem::as.cas(casrn)
+    unit = 'ug/l'
   )
 
 
@@ -95,22 +121,25 @@ oe <- read_html('https://www.epa.gov/wqc/national-recommended-water-quality-crit
   pluck(., 1) %>% 
   `colnames<-`(., c('analyte', 'casrn', 'result')) %>% 
   mutate(
-    unit = 'ug/l',
-    cas_chk = webchem::as.cas(casrn)
+    unit = 'ug/l'
   ) %>% 
   filter(analyte != '1Non-Priority pollutant.') %>% 
   rename(value = result)
 
-raw <- list_rbind(list('alc' = alc, 'hhc' = hhc, 'oe' = oe), names_to = 'frame')
-
-rm(alc, hhc, oe)
-
 # cleaning ----------------------------------------------------------------
 
+
+raw <- list_rbind(list('dw' = dw, 'alc' = alc, 'hhc' = hhc, 'oe' = oe), names_to = 'frame')
+
+write_rds(raw, 'raw.RDS')
+
+rm(alc, hhc, oe, dw)
+
 raw_dat <- raw %>% 
-  distinct(analyte, casrn, cas_chk, .keep_all = F) %>% 
+  distinct(analyte, casrn, .keep_all = F) %>% 
   mutate(
     idx = 1:n(),
+    cas_chk = webchem::as.cas(casrn),
     raw_name = str_remove_all(analyte, pattern = "\\([^()]*\\)") %>% str_squish()
   )
 
@@ -127,6 +156,8 @@ bad_src <- bad_search %>%
 bad_dat <- inner_join(bad, bad_src, join_by(raw_name == raw_search)) %>% 
   select(idx, dtxsid)
 
+rm(bad, bad_search, bad_src)
+
 good <- raw_dat %>% 
   filter(!is.na(cas_chk)) %>% 
   mutate(cas_chk = unname(cas_chk))
@@ -140,169 +171,206 @@ good_src <- good_cas %>%
 good_dat <- inner_join(good, good_src, join_by(cas_chk == raw_search)) %>% 
   select(idx, dtxsid)
 
+rm(good, good_cas, good_src)
+
 raw_cur <- bind_rows(list(good_dat, bad_dat)) %>% 
   left_join(raw_dat, ., join_by(idx)) %>% 
-  select(-raw_name) %>% 
+  select(-raw_name, -casrn, -cas_chk) %>% 
   arrange(dtxsid)
-
-rm(
-  list = setdiff(ls(), "raw_cur")
-)
-
 
 # hashing -----------------------------------------------------------------
 {
-params <- raw_cur %>% 
-  filter(is.na(dtxsid)) %>% 
-  select(analyte) %>% 
-  mutate(raw_search = str_remove_all(analyte, pattern = '\\*') %>%
-                    str_remove_all(., pattern = '\\(P\\)') %>% 
-                    str_remove_all(., pattern = "\\([^()]*\\)") %>% 
-                    str_squish())
-
-chars <- rio::import(here('dict','Characteristic.csv')) %>% 
-  clean_names() %>% 
-  select(name, cas_number, srs_id)
-
-{
-# srs_b <- map(params$raw_search, ~srs_search(query = .x, method = 'begins'), .progress = T) %>% 
-#     set_names(., params$raw_search) %>% 
-#     list_rbind(names_to = 'raw_search')
+  params <- raw_cur %>% 
+    filter(is.na(dtxsid)) %>% 
+    select(idx, analyte) %>% 
+    mutate(raw_search = str_remove_all(analyte, pattern = '\\*') %>%
+             str_remove_all(., pattern = '\\(P\\)') %>% 
+             str_remove_all(., pattern = "\\s*\\([^()]*\\)$") %>% 
+             str_remove_all(., pattern = "\\s*\\([^()]*\\)$") %>% 
+             str_squish())
   
-srs_e <- map(params$raw_search, ~srs_search(query = .x, method = 'exact'), .progress = T) %>% 
-  set_names(., params$raw_search) %>% 
-  list_rbind(names_to = 'raw_search')
-  
-srs_e_details <- map(srs_e$itn, ~srs_details(query = .x), .progress = T) %>% 
-  set_names(., srs_e$raw_search) %>% 
-  list_rbind(names_to = 'raw_search') %>% 
-  group_by(raw_search) %>% 
-  arrange(desc(synonyms)) %>% 
-  ungroup() %>% 
-  distinct(raw_search, .keep_all = T) 
-
-missing <- params %>% 
-  filter(!raw_search %in% srs_e_details$raw_search) %>% 
-  select(raw_search) %>% 
-  unlist()
-
-srs_c <- map(missing, ~srs_search(query = .x, method = 'contains'), .progress = T) %>% 
-  set_names(., missing) %>% 
-  list_rbind(names_to = 'raw_search')
-
-srs_c_details <- map(srs_c$itn, ~srs_details(query = .x), .progress = T) %>% 
-  set_names(., srs_c$raw_search) %>% 
-  list_rbind(names_to = 'raw_search') %>% 
-  group_by(raw_search) %>% 
-  arrange(desc(synonyms)) %>% 
-  ungroup() %>% 
-  distinct(raw_search, .keep_all = T) 
-
-rm(srs_e, srs_c)
-
-params_cur <- list(srs_e_details, srs_c_details) %>%
-  list_rbind() %>% 
-  select(
-    raw_search, 
-    internalTrackingNumber, 
-    systematicName, 
-    currentCasNumber,
-    dtxsid
-  ) %>% 
-  `colnames<-`(., c(
-    'raw_search',
-    'itn',
-    'preferredName',
-    'casrn',
-    'dtxsid'
-    ))
-
-#TODO need to join against raw names for final list. 
-
-# missing <- params %>% 
-#   anti_join(., params_cur, join_by(raw_search))
-  }
+  {
+    
+    srs_e <- map(params$raw_search, ~srs_search(query = .x, method = 'exact'), .progress = T) %>% 
+      set_names(., params$raw_search) %>% 
+      list_rbind(names_to = 'raw_search')
+    
+    srs_e_details <- map(srs_e$itn, ~srs_details(query = .x), .progress = T) %>% 
+      set_names(., srs_e$raw_search) %>% 
+      list_rbind(names_to = 'raw_search') %>% 
+      group_by(raw_search) %>% 
+      arrange(desc(synonyms)) %>% 
+      ungroup() %>% 
+      distinct(raw_search, .keep_all = T) 
+    
+    missing <- params %>% 
+      filter(!raw_search %in% srs_e_details$raw_search) %>% 
+      select(raw_search) %>% 
+      unlist() %>% 
+      unname() %>% 
+      print()
+    
+    srs_c <- map(missing, ~srs_search(query = .x, method = 'contains'), .progress = T) %>% 
+      set_names(., missing) %>% 
+      list_rbind(names_to = 'raw_search')
+    
+    srs_c_details <- map(srs_c$itn, ~srs_details(query = .x), .progress = T) %>% 
+      set_names(., srs_c$raw_search) %>% 
+      list_rbind(names_to = 'raw_search') %>% 
+      group_by(raw_search) %>% 
+      arrange(desc(synonyms)) %>% 
+      ungroup() %>% 
+      distinct(raw_search, .keep_all = T) 
+    
+    missing <- params %>% 
+      filter(!raw_search %in% c(srs_c_details$raw_search, srs_e_details$raw_search)) %>% 
+      #select(raw_search) %>% 
+      #unlist() %>% 
+      #unname() %>% 
+      print()
+    
+    #NOTE Manual process here!
+    missing <- missing %>% 
+      mutate(itn = case_when(
+        raw_search == 'Hazard Index PFAS' ~ '1995414',
+        raw_search == 'Radium 226 and Radium 228' ~ '701037'
+      )) %>% 
+      select(-analyte, -idx)
+    
+    missing_src <- map(missing$itn, ~srs_details(query = .x)) %>% 
+      list_rbind()
+    
+    missing_dat <- missing %>% 
+      left_join(., missing_src, join_by(itn == internalTrackingNumber)) %>% 
+      rename(internalTrackingNumber = itn)
+    
+    rm(srs_e, srs_c)
+    
+    params_cur <- list(missing_dat, srs_e_details, srs_c_details) %>%
+      list_rbind() %>% 
+      select(
+        raw_search, 
+        internalTrackingNumber, 
+        systematicName, 
+        currentCasNumber,
+        dtxsid
+      ) %>% 
+      `colnames<-`(., c(
+        'raw_search',
+        'itn',
+        'preferredName',
+        'casrn',
+        'dtxsid'
+      )) 
+    
+    #TODO need to join against raw names for final list. 
+    
+    missing <- params %>%
+      anti_join(., params_cur, join_by(raw_search))
+    
+    }
 }
-# 
-# hash <- 
-#   map(
-#     c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw"), 
-#     ~ {stringdist_join(params_hash, chars,
-#                    by = c('raw_search' = 'name'),
-#                    mode = 'right',
-#                    method = .x,
-#                    #max_dist = 9,
-#                    distance_col = 'dist') %>% 
-#       group_by(analyte) %>% 
-#       filter(!is.na(dist)) %>% 
-#       slice_min(order_by = dist, n = 5)
-#       },
-#     .progress = T) %>% 
-#   set_names(., c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw"))
-# 
-# #TODO Repeat this as each method for comparison...? 
-# {
-# hash_1 <- 
-#   keep(hash, names(hash) %in% c("osa", "lv", "dl", "hamming", "lcs", "qgram")) %>% 
-#   list_rbind(names_to = 'method') %>% 
-#   filter(!is.na(srs_id)) %>% 
-#   distinct(analyte, name, cas_number, srs_id, .keep_all = T) %>% 
-#   select(-c(raw_search, method, dist)) %>% 
-#   mutate(srs_id = as.character(srs_id))
-# 
-# hash_2 <- 
-#   keep(hash, names(hash) %in% c("cosine", "jaccard", "jw")) %>% 
-#   list_rbind(names_to = 'method') %>% 
-#   group_by(analyte) %>% 
-#   slice_min(dist, n = 1) %>% 
-#   filter(!(analyte %in% hash_1$analyte)) %>% 
-#   distinct(analyte, name, cas_number, srs_id, .keep_all = T) %>% 
-#   filter(dist <= 0.1) %>% 
-#   select(-c(raw_search, method, dist)) %>% 
-#   ungroup() %>% 
-#   mutate(srs_id = as.character(srs_id))
-# 
-# #NOTE Unlikely to produce any good results
-# 
-# 
-# {
-#   hash_3 <- 
-#     keep(hash, names(hash) %in% c("cosine", "jaccard", "jw")) %>% 
-#     list_rbind(names_to = 'method') %>% 
-#     filter(!c(analyte %in% c(hash_1$analyte, hash_2$analyte))) %>% 
-#     group_by(analyte) %>% 
-#     slice_min(dist, n = 1) %>% 
-#     distinct(analyte, raw_search)
-#   
-#   hash_4 <- map(hash_3$raw_search, ~srs_search(query = .x, method = 'begins'), .progress = T) %>% 
-#     set_names(., hash_3$analyte) %>% 
-#     list_rbind(names_to = 'raw_search') %>% 
-#     select(raw_search, itn)
-# 
-#   hash_4 <- map(hash_4$itn, ~srs_details(query = .x), .progress = T) %>% 
-#     set_names(., hash_4$raw_search) %>% 
-#     list_rbind(names_to = 'raw_search') %>% 
-#     select(raw_search, systematicName, internalTrackingNumber) %>% 
-#     rename(
-#       name = systematicName,
-#       srs_id = internalTrackingNumber
-#     ) %>% 
-#     mutate(name = str_to_title(name))
-#   
-#   hash_3 <- hash_3 %>% 
-#     left_join(., hash_4, join_by(raw_search)) %>% 
-#     select(-raw_search) %>% 
-#     ungroup() %>% 
-#     mutate(srs_id = as.character(srs_id))
-#   
-#   rm(hash_4)
-#   } 
-# 
-#   params <- list_rbind(list(hash_1, hash_2, hash_3)) %>% 
-#     select(-cas_number)
-#   
-#   rm(hash_1, hash_2, hash_3, hash, chars)
-#   }
-# }
 
+params_fin <- params_cur %>% 
+  left_join(params, ., join_by(raw_search)) %>% 
+  select(-casrn, -raw_search)
+
+raw_cur <- raw_cur %>% 
+  filter(!is.na(dtxsid))
+
+final <- bind_rows(params_fin, raw_cur) %>% 
+  mutate(
+    preferredName = case_when(
+      !is.na(dtxsid) ~ NA,
+      is.na(dtxsid) ~ preferredName)
+  )
+
+dss <- final %>% 
+  filter(!is.na(dtxsid)) %>% 
+  select(-preferredName, -itn)
+
+#NOTE Some duplicates here
+dss_cur <- ct_details(dss$dtxsid) %>% 
+  select(-casrn)
+
+dss <- dss %>% 
+  left_join(., dss_cur, join_by(dtxsid))
+
+wq <- final %>% 
+  filter(!idx %in% dss$idx) %>% 
+  mutate(dtxsid = paste0('E', itn)) %>% 
+  select(-itn) %>% 
+  mutate(preferredName = str_remove_all(preferredName, pattern = ', from SDWA NPDWR|-- SWDA NPDWR'))
+
+final_dictionary <- bind_rows(wq, dss) %>% 
+  arrange(idx) %>% 
+  select(-idx) %>% 
+  distinct(.keep_all = T)
+
+
+# Final pairing -----------------------------------------------------------
+
+ndwqs <- raw %>% 
+  select(-casrn) %>%
   
+  # NOTE debugging
+  # anti_join(., final_dictionary, join_by(analyte)) %>% 
+  
+  left_join(., final_dictionary, join_by(analyte)) %>% 
+  select(-year) %>% 
+  mutate(
+    idx = 1:n(),
+    source = if_else(is.na(source), 'ND', source),
+    notes = case_when(
+      notes == '' ~ NA,
+      .default = notes
+    ),
+    orig_value = value,
+    value = str_remove_all(value, pattern = '\\u2014|ug/L|,|--|---|none|vacated|NP1'), 
+    value = str_remove_all(value, pattern = 'TT.*|n/a.*'),
+    value = str_squish(value),
+    value = str_replace_all(value, pattern = 'zero', replacement = "0"),
+    value = na_if(value, "")
+  ) %>% filter(!is.na(value))
+
+n_1 <- ndwqs %>% 
+  mutate(value = as.numeric(value)) %>% 
+  filter(!is.na(value))
+
+n_2 <- ndwqs %>% 
+  filter(idx %ni% n_1$idx) %>% 
+  filter(source != 'Disinfectants', source != 'Selected Per- and poly-fluoroalkyl substances (PFAS)') %>% 
+  arrange(preferredName) %>% 
+  mutate(
+    unit = case_when(
+      preferredName == 'Alpha particle'  ~ 'pCi/L',
+      preferredName == 'Asbestos' ~ 'million fibers/L',
+      preferredName == 'Beta Particles And Photon Emitters' ~ 'mrem/yr',
+      preferredName == 'Methyl mercury(II) cation' ~ 'mg/kg',
+      preferredName == 'Radium, isotope of mass 226 and/or radium, isotope of mass 228' ~ 'pCi/L',
+      preferredName == 'Total coliforms' ~ '%',
+      .default = unit
+    ),
+    value = case_when(
+      preferredName == 'Alpha particle'  ~ '15',
+      preferredName == 'Asbestos' ~ '7',
+      preferredName == 'Arsenic' ~ '0.01',
+      preferredName == 'Beta Particles And Photon Emitters' ~ '4',
+      preferredName == 'Methyl mercury(II) cation' ~ '0.3',
+      preferredName == 'Radium, isotope of mass 226 and/or radium, isotope of mass 228' ~ '5',
+      preferredName == 'Total coliforms' ~ '5',
+      preferredName == 'Perfluorooctanesulfonic acid' ~ '0.25',
+      preferredName == 'Perfluorooctanoic acid' ~ '100',
+      preferredName == 'Uranium' ~ '30',
+      .default = value
+    ),
+    value = as.numeric(value)
+  ) %>% 
+  filter(!is.na(value))
+
+
+#NOTE debugging
+ndwqs %>%
+  filter(idx %ni% c(n_1$idx, n_2$idx)) %>% 
+  filter(source != 'Disinfectants', source != 'Selected Per- and poly-fluoroalkyl substances (PFAS)')
+
