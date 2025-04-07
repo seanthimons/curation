193ea9843be95d2a7c4c03ec6f72bd97ed0b4a54
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-04-07 17:58:30 -0400

    Create scrap.R

---
+++ threat_build/scrap.R
@@ -0,0 +1,56 @@
+dbListTables(threat_db)
+
+tbl(threat_db, "toxval_9_6") %>%
+  #filter(DTXSID == 'DTXSID2021238') %>% 
+  glimpse()
+
+cwa <- ct_list('CWA311HS') %>% pluck(.,1)
+
+{
+tbl(threat_db, "toxval_9_6") %>%
+  filter(
+    SOURCE != "ECOTOX",
+    DTXSID %in% cwa$dtxsids,
+    SPECIES_COMMON == 'Human',
+    EXPOSURE_ROUTE == "oral",
+    RISK_ASSESSMENT_CLASS == 'Water',
+    TOXVAL_UNITS == "mg/L",
+  ) %>%
+  group_by(
+    DTXSID,
+    TOXVAL_TYPE,
+    SOURCE,
+  ) %>%
+  summarize(
+    TOXVAL_NUMERIC = min(as.numeric(TOXVAL_NUMERIC), na.rm = TRUE)
+    ,UNITS = first(TOXVAL_UNITS)
+    ) %>%
+  arrange(desc(TOXVAL_NUMERIC)) %>%
+  head(n = 20) %>% 
+  collect() %>% 
+  print(n = Inf)
+}
+
+{
+tbl(threat_db, "toxval_9_6") %>%
+  filter(
+    SOURCE == "ECOTOX",
+    DTXSID %in% cwa$dtxsids,
+    SPECIES_COMMON == 'Human',
+    EXPOSURE_ROUTE == "oral",
+    RISK_ASSESSMENT_CLASS == 'Water',
+    TOXVAL_UNITS == "mg/L",
+  ) %>%
+  group_by(
+    DTXSID,
+    TOXVAL_TYPE,
+    SOURCE,
+  ) %>%
+  summarize(
+    TOXVAL_NUMERIC = min(as.numeric(TOXVAL_NUMERIC), na.rm = TRUE)
+    ,UNITS = first(TOXVAL_UNITS)
+  ) %>%
+  arrange(desc(TOXVAL_NUMERIC)) %>%
+  head(n = 20) %>% 
+  collect() %>% 
+  print(n = Inf)}
