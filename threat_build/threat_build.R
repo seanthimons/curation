c076ffafcd4a17d406b594e30f6f546166b70dd7
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-04-07 17:58:26 -0400

    Create threat_build.R

---
+++ threat_build/threat_build.R
@@ -0,0 +1,87 @@
+
+# Packages ----------------------------------------------------------------
+
+{
+  library(here)
+  library(rio)
+  library(janitor)
+  library(tidyverse)
+  library(ComptoxR)
+  library(arrow)
+  library(duckdb)
+  library(duckplyr)
+  
+  
+  setwd(here('threat_build'))
+  
+}
+
+
+# functions ---------------------------------------------------------------
+
+query_db <- function(...){
+  variables <- rlang::ensyms(...)
+  
+  dbListTables(threat_db) %>%
+    map(., ~{
+      tbl(threat_db, .x) %>%
+        distinct(
+          !!!variables
+        ) %>%
+        collect()
+      
+    }, .progress = TRUE) %>%
+    list_rbind() %>% distinct()
+}
+
+pull_table <- function(...){
+  
+  variables <- rlang::ensyms(...)
+  
+  dbListTables(threat_db) %>%
+    map(., ~{
+      tbl(threat_db, .x) %>% 
+        select(!!!variables) %>% 
+        collect()
+    }, .progress = TRUE) %>%
+    list_rbind()
+}
+
+# init --------------------------------------------------------------------
+
+if(file.exists('threat.duckdb')){
+  
+  #threat_db <- dbConnect(duckdb(), dbdir = 'threat.duckdb', read_only = FALSE)
+  
+  #dbListTables(threat_db)
+  
+  
+}else{
+  
+  final_lof <- list.files(here('final'))
+  
+  final_lof <- final_lof %>% 
+    str_subset(., pattern = '.RDS|bayes|treatment', negate = TRUE)
+  
+  final_lof
+  
+  threat_db <- 
+    dbConnect(duckdb(), dbdir = ":memory:", read_only = FALSE)
+    #dbConnect(duckdb(), dbdir = 'threat.duckdb', read_only = FALSE)
+  
+  final_lof %>% 
+    walk(., function(x){
+      cli::cli_alert_info(x, '\n')
+      basename(x)
+      dbWriteTable(threat_db, str_remove(x, pattern = '.parquet'), read_parquet(here('final', x)), overwrite = TRUE)
+      
+    }, .progress = TRUE)
+  
+  dbListTables(threat_db)
+}
+
+
+
+
+
+
