3e2ddfe910e339fc85abedcecc690389198f08d7
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-05-09 17:26:29 -0400

    Update scratch.R

--- scratch.R
+++ scratch.R
@@ -43,3 +43,64 @@ hash_1 <-
       list_rbind(names_to = "method") %>% 
       filter(
         method == 'cosine' & dist <= quantile(.[.$method == 'cosine', ][['dist']], 0.015))
+
+
+
+job::job({
+  
+  library(rgbif)
+  
+  
+  
+  sp_dat <- sp$latin %>% 
+    map(., ~{
+      name_backbone(name = .x) %>% 
+        as_tibble()
+    }, .progress = TRUE) %>% 
+    set_names(sp$latin) %>% 
+    list_rbind(names_to = 'raw_search') %>% 
+    filter(!is.na(usageKey)) %>% 
+    inner_join(sp, ., join_by(latin == raw_search))
+  
+  hab_dat_raw <- map(sp_dat$usageKey, ~name_usage(key = .x, data = 'speciesProfiles'), .progress = TRUE) %>% 
+    set_names(sp_dat$usageKey)
+  
+  hab_dat <- hab_dat_raw %>% 
+    map(., ~{
+      pluck(., 'data')
+    }) %>% 
+    compact() %>% 
+    list_rbind(names_to = 'usageKey') %>% 
+    select(
+      usageKey, 
+      marine, 
+      freshwater
+      #choosing to not use this as I don't need it? 
+      #, terrestrial 
+      #choosing to not use this since it is too freeform
+      #,habitat
+    ) %>% 
+    pivot_longer(., cols = c(marine, freshwater), values_to = 'hab1', values_drop_na = TRUE) %>% 
+    distinct() %>% 
+    arrange(hab1) %>%
+    distinct(usageKey, name, .keep_all = TRUE) %>% 
+    pivot_wider(names_from = name, values_from = hab1, values_fill = FALSE) %>% 
+    mutate(usageKey = as.integer(usageKey)) %>% 
+    inner_join(
+      sp_dat %>% 
+        select(
+          latin,
+          common, 
+          usageKey,
+          canonicalName
+        ),
+      .,
+      join_by(usageKey)) %>% 
+    distinct() %>% 
+    mutate(hab_chk = case_when(
+      freshwater == TRUE & marine == TRUE ~ TRUE,
+      .default = FALSE
+    ))
+  
+})
+
