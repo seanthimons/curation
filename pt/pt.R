782559c348911593cb274cca9bf3eb5e29175c1b
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-07-25 11:09:33 -0400

    Update pt.R

--- pt/pt.R
+++ pt/pt.R
@@ -1,122 +1,172 @@
 # packages ----------------------------------------------------------------
 {
-  library(here)
-  library(tidyverse)
-  library(lubridate)
-  library(janitor)
-  library(rvest)
-  library(httr)
-  library(polite)
-  library(rio)
-  library(ComptoxR)
+	library(here)
+	library(tidyverse)
+	library(lubridate)
+	library(janitor)
+	library(rvest)
+	library(httr)
+	library(polite)
+	library(rio)
+	library(ComptoxR)
 
-  setwd(here('pt'))
+	setwd(here('pt'))
 
-  `%ni%` <- Negate(`%in%`)
+	`%ni%` <- Negate(`%in%`)
 }
+
+
+# DSSTox db --------------------------------------------------------------
+
+{
+	library(duckdb)
+
+	dsstox_db <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
+
+	list.files(
+		here("final"),
+		pattern = "dsstox_.*\\.parquet$",
+		full.names = TRUE
+	) %>%
+		walk(
+			.,
+			~ {
+				table_name <- basename(.x) %>%
+					str_extract(., pattern = 'dsstox')
+				# str_remove("\\.parquet$") %>%
+				# str_extract(., pattern = '^dsstox_.*') # Extract full table name like "dsstox_chemicals"
+
+				# Instead of reading into R memory, directly create a table from the parquet file
+				dbExecute(
+					dsstox_db,
+					paste0("CREATE TABLE ", table_name, " AS SELECT * FROM '", .x, "'")
+				)
+			}
+		)
+
+	# NOTE adjusted due to duckdb limitations on regex pattern
+	isotope_regex_pattern <- paste0(
+		"(^\\d+[A-Z][a-z]?)|",
+		"(^\\[\\d+[A-Z][a-z]?\\](?:[1-9]\\d*)?$)|",
+		"(\\b[D|T]\\b)"
+	)
+}
+
+
+# PT list  ---------------------------------------------------------------
 
 pt <- list()
 
 # oxidation states --------------------------------------------------------
 
 ind_html <-
-  polite::bow(
-    'https://en.wikipedia.org/wiki/Oxidation_state#List_of_oxidation_states_of_the_elements'
-  ) %>%
-  polite::scrape(.) %>% # scrape web page
-  rvest::html_nodes("table.wikitable") %>% # pull out specific table
-  rvest::html_table()
+	polite::bow(
+		'https://en.wikipedia.org/wiki/Oxidation_state#List_of_oxidation_states_of_the_elements'
+	) %>%
+	polite::scrape(.) %>% # scrape web page
+	rvest::html_nodes("table.wikitable") %>% # pull out specific table
+	rvest::html_table()
 
 #TODO Perhaps add a Roman Numeral column?
 
 pt$oxidation_state <- ind_html[[3]] %>%
-  .[4:121, 1:19] %>%
-  setNames(., LETTERS[1:19]) %>%
-  as_tibble() %>%
-  mutate(
-    I = as.character(I),
-    A = as.numeric(A)
-  ) %>%
-  mutate(across(B:S, ~ na_if(., ""))) %>%
-  pivot_longer(
-    .,
-    cols = D:R,
-    values_to = 'oxidation_state',
-    values_drop_na = T
-  ) %>%
-  select(-name) %>%
-  setNames(., c('number', 'element', 'symbol', 'group', 'oxs')) %>%
-  mutate(
-    ox = case_when(
-      stringr::str_detect(oxs, "\\+") ~ "+",
-      stringr::str_detect(oxs, "\\u2212") ~ "-",
-      .default = " "
-    ),
-    oxs = stringr::str_remove_all(oxs, "[[:symbol:]]"),
-    oxidation_state = paste0(oxs, ox) %>% str_trim(),
-    search = case_when(
-      oxidation_state == '0' ~ symbol,
-      oxidation_state == '1-' ~ paste0(symbol, ox),
-      oxidation_state == '1+' ~ paste0(symbol, ox),
-      .default = paste0(symbol, oxidation_state)
-    )
-  ) %>%
-  select(!c(oxs:ox))
+	.[4:121, 1:19] %>%
+	setNames(., LETTERS[1:19]) %>%
+	as_tibble() %>%
+	mutate(
+		I = as.character(I),
+		A = as.numeric(A)
+	) %>%
+	mutate(across(B:S, ~ na_if(., ""))) %>%
+	pivot_longer(
+		.,
+		cols = D:R,
+		values_to = 'oxidation_state',
+		values_drop_na = T
+	) %>%
+	select(-name) %>%
+	setNames(., c('number', 'element', 'symbol', 'group', 'oxs')) %>%
+	mutate(
+		ox = case_when(
+			stringr::str_detect(oxs, "\\+") ~ "+",
+			stringr::str_detect(oxs, "\\u2212") ~ "-",
+			.default = " "
+		),
+		oxs = stringr::str_remove_all(oxs, "[[:symbol:]]"),
+		oxidation_state = paste0(oxs, ox) %>% str_trim(),
+		search = case_when(
+			# TODO Add proper parsing for "2+" -> "++"", "2-" -> "--"
+			oxidation_state == '0' ~ symbol,
+			oxidation_state == '1-' ~ paste0(symbol, ox),
+			oxidation_state == '1+' ~ paste0(symbol, ox),
+			.default = paste0(symbol, oxidation_state)
+		)
+	) %>%
+	select(!c(oxs:ox))
+
+# ? NOTE Holding off on this for now, little to no benefit.
+# pt_elements <- pt$oxidation_state %>%
+# 	select(symbol) %>%
+# 	distinct()
+
+# ox_dsstox <- tbl(dsstox_db, 'dsstox') %>%
+# 	select(DTXSID, MOLECULAR_FORMULA, SMILES, CASRN) %>%
+# 	collect() %>%
+# 	inner_join(., pt_elements, join_by(MOLECULAR_FORMULA == symbol))
+# 	filter(!str_detect(SMILES, pattern = "\\+|\\-"))
 
 rm(ind_html)
 
 # Table -------------------------------------------------------------------
 
 pt$elements <- bow(
-  'https://en.wikipedia.org/wiki/List_of_chemical_elements'
+	'https://en.wikipedia.org/wiki/List_of_chemical_elements'
 ) %>%
-  scrape(content = "text/html; charset=UTF-8") %>%
-  html_nodes(".wikitable") %>%
-  html_table() %>%
-  pluck(1) %>%
-  set_names(
-    .,
-    c(
-      'Number',
-      'Symbol',
-      'Name',
-      'Origin of name',
-      'Group',
-      'Period',
-      'Block',
-      'Standard atomic weight',
-      'Density',
-      'Melting point',
-      'Boiling point',
-      'Specific heat capacity',
-      'Electro negativity',
-      'Abundance',
-      'Origin',
-      'Phase'
-    )
-  ) %>%
-  slice(., 4:nrow(.)) %>%
-  mutate(Number = as.numeric(Number))
+	scrape(content = "text/html; charset=UTF-8") %>%
+	html_nodes(".wikitable") %>%
+	html_table() %>%
+	pluck(1) %>%
+	set_names(
+		.,
+		c(
+			'Number',
+			'Symbol',
+			'Name',
+			'Origin of name',
+			'Group',
+			'Period',
+			'Block',
+			'Standard atomic weight',
+			'Density',
+			'Melting point',
+			'Boiling point',
+			'Specific heat capacity',
+			'Electro negativity',
+			'Abundance',
+			'Origin',
+			'Phase'
+		)
+	) %>%
+	slice(., 4:nrow(.)) %>%
+	mutate(Number = as.numeric(Number))
 
-pt_dtxsid <- ComptoxR::ct_search(
-  type = 'string',
-  search_param = 'equal',
-  query = pt$elements$Name
-)
+pt_smiles <- pt$elements %>%
+	select(Symbol)
 
-pt_dtxsid_final <- pt_dtxsid %>%
-  arrange(., rank) %>%
-  distinct(searchValue, .keep_all = T) %>%
-  select(dtxsid, casrn, preferredName, searchValue)
+pt_dsstox <- tbl(dsstox_db, 'dsstox') %>%
+	select(DTXSID, MOLECULAR_FORMULA, SMILES, CASRN) %>%
+	collect() %>%
+	inner_join(., pt_smiles, join_by(MOLECULAR_FORMULA == Symbol)) %>%
+	filter(!str_detect(SMILES, pattern = "\\+|\\-"))
 
-pt$elements <- left_join(
-  pt$elements,
-  pt_dtxsid_final,
-  by = c('Name' = 'searchValue')
-) %>%
-  mutate(Number = as.character(Number))
+pt$elements <-
+	left_join(
+		pt$elements,
+		pt_dsstox,
+		join_by(Symbol == MOLECULAR_FORMULA)
+	) %>%
+	mutate(Number = as.character(Number))
 
-rm(pt_dtxsid, pt_dtxsid_final)
 
 # Nuclides and isotopes ----------------------------------------------------------------
 
@@ -157,33 +207,42 @@ rm(pt_dtxsid, pt_dtxsid_final)
 #   distinct(isotopes, .keep_all = T)
 
 nuclides <- bow('https://en.wikipedia.org/wiki/Table_of_nuclides') %>%
-  scrape(content = "text/html; charset=UTF-8") %>%
-  html_nodes(".wikitable") %>%
-  html_table() %>%
-  pluck(., 3) %>%
-  .[-1, -1] %>%
-  `colnames<-`(., NULL) %>%
-  as_tibble(., .name_repair = 'universal') %>%
-  mutate_all(~ na_if(., "")) %>%
-  pivot_longer(
-    .,
-    cols = everything(),
-    names_to = 'col',
-    values_to = 'iso',
-    values_drop_na = T
-  ) %>%
-  filter(iso %ni% pt$elements$Number, iso %ni% pt$elements$Symbol) %>%
-  mutate(
-    Z = str_extract_all(iso, pattern = "\\d+(?:\\.\\d+)?"),
-    element = str_extract_all(iso, pattern = "[^0-9.]+")
-  ) %>%
-  select(-iso, -col) %>%
-  unnest(., cols = c(Z, element))
+	scrape(content = "text/html; charset=UTF-8") %>%
+	html_nodes(".wikitable") %>%
+	html_table() %>%
+	pluck(., 3) %>%
+	.[-1, -1] %>%
+	`colnames<-`(., NULL) %>%
+	as_tibble(., .name_repair = 'universal') %>%
+	mutate_all(~ na_if(., "")) %>%
+	pivot_longer(
+		.,
+		cols = everything(),
+		names_to = 'col',
+		values_to = 'iso',
+		values_drop_na = T
+	) %>%
+	filter(iso %ni% pt$elements$Number, iso %ni% pt$elements$Symbol) %>%
+	mutate(
+		Z = str_extract_all(iso, pattern = "\\d+(?:\\.\\d+)?"),
+		element = str_extract_all(iso, pattern = "[^0-9.]+")
+	) %>%
+	select(-iso, -col) %>%
+	unnest(., cols = c(Z, element)) %>% 
+	mutate(
+		smiles = paste0("[", Z, element, "]")
+	) %>% 
+	left_join(
+		.,
+		tbl(dsstox_db, 'dsstox') %>%
+			select(DTXSID, CASRN, SMILES) %>%
+			filter(str_detect(SMILES, isotope_regex_pattern)) %>%
+			collect(),
+		join_by(smiles == SMILES)
+	)
 
 pt$isotopes <- nuclides
 
-q1 <- chem
-
 # Export ------------------------------------------------------------------
 
-rio::export(pt, file = 'pt.RDS')
+rio::export(pt, file = here('final', 'pt.RDS'))
