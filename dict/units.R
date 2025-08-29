6706f5ce081f49754d9fa422bfb7767678888bc1
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-08-29 14:34:16 -0400

    Add script to process unit dictionaries
    Introduces dict/units.R to import, clean, and save unit and conversion data from CSV files into an RDS file for later use. This script uses several tidyverse and utility packages for data processing.

---
+++ dict/units.R
@@ -0,0 +1,31 @@
+# packages ----------------------------------------------------------------
+
+{
+  library(here)
+  library(rio)
+  library(tidyverse)
+  library(janitor)
+  library(stringdist)
+  library(fuzzyjoin)
+  library(todor)
+
+  setwd(here('dict'))
+
+  todor::todor()
+}
+
+
+# raw ---------------------------------------------------------------------
+
+# https://apps.ecology.wa.gov/eim/help/ValidValues/
+
+preferred_units <- rio::import(file = here('dict', 'Units.csv')) %>%
+  janitor::clean_names()
+
+unit_conv <- rio::import(file = here('dict', 'UOMConversion.csv')) %>%
+  janitor::clean_names() %>%
+  select(3:6)
+
+unit_dict <- list(preferred_units = preferred_units, unit_conv = unit_conv)
+
+saveRDS(unit_dict, file = here('final', 'unit_dict.RDS'))
